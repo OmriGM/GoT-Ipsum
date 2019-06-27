02e5db670d0a4ca5251bbdcd69410287e9360a9e
OmriGM <omrigr123@gmail.com>
2019-06-27 21:56:52 +0300

    Added Search and Header components

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import './Header.scss'
+
+class Header extends Component {
+    render() {
+        return (
+            <div className='header'>
+               GoT Lorem Ipsum
+            </div>
+        )
+    }
+}
+
+export default Header
